674cefa72877c017e5e4c89857986cf12c31f8b7
Yarosh Darya <gurimusagi@yandex.ru>
2022-01-29 08:14:09 +0300

    Create "App"
    * Deleted default elements;
    * Added new elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CreditParameters from './components/creditParameters';
+import MainResults from './components/mainResults';
+import RefinancingDetails from './components/refinancingDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Кредитный калькулятор</h1>
       </header>
+      <main>
+        <CreditParameters />
+        <MainResults />
+        <RefinancingDetails />
+      </main>
     </div>
   );
 }
